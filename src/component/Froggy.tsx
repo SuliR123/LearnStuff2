8359808c0beeb19e9bba83271db292fba9274e47
justagoodfriend <greenate1@gmail.com>
2023-01-29 15:34:02 -0500

    idk why theres more to commit

--- src/component/Froggy.tsx
+++ src/component/Froggy.tsx
@@ -1,10 +1,9 @@
 export const Froggy: React.FC = () => {
-    //style this with CSS later
-    //again the same issue with the div not taking up the entire space
+    //in the future, if I want to seet the box/etc to a certain heigh/width wrapt within a bracket!
     return (
         <div className="flex h-screen w-screen justify-center items-center">
             <div >
-                <img className="h-500px w-500px"  src="https://cdn.dribbble.com/users/1044993/screenshots/5945543/media/4f75b2ce7a66bb8f3704f7fcbb40d8f9.png?compress=1&resize=400x300&vertical=top"></img>
+                <img className="h-[500px] w-[500px]"  src="https://cdn.dribbble.com/users/1044993/screenshots/5945543/media/4f75b2ce7a66bb8f3704f7fcbb40d8f9.png?compress=1&resize=400x300&vertical=top"></img>
             </div>
         </div>
     );
